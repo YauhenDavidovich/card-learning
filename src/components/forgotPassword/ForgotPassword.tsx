4722eec3877b8bd7e8a53f01edfd29a0c08f4b0f
Yauhen Davidovich <davidovich336@gmail.com>
2021-11-13 21:30:27 +0100

    refactor: change request password component name

---
+++ src/components/forgotPassword/ForgotPassword.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+
+    return (
+        <div className={"main"}>ForgotPassword?</div>
+
+    )
+}
+
+export default ForgotPassword
+;
---
+++ src/components/notFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+
+    return (
+        <div className={"main"}>Page not found</div>
+
+
+
+    )
+}
+
+export default Main;
