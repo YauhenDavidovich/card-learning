ff5c17d6fed856639695f9b44881514ef3ae3b14
Yauhen Davidovich <davidovich336@gmail.com>
2021-11-14 10:43:25 +0100

    feat: add password api

---
+++ src/dll/restorePasswordApi.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: process.env.API_PATH,
+    withCredentials: true
+});
+
+export const restorePasswordApi = {
+
+    requestForgotPassword(data: ForgotRequestDataType) {
+        return instance.post<CheckEmailResponseType>('auth/forgot', data )
+    },
+    setNewPassword(data: NewPasswordRequestType) {
+        return instance.post<NewPasswordResponseType>('auth/set-new-password', data)
+    }
+}
+
+export type ForgotRequestDataType = {
+    email: string
+    from: string
+    message: string
+}
+
+export type CheckEmailResponseType = {
+    info: string | null
+    answer: boolean
+    html: boolean
+    success: boolean
+}
+
+export type NewPasswordRequestType = {
+    password: string
+    resetPasswordToken: string
+}
+
+export type NewPasswordResponseType = {
+    info: string
+    error: string
+}
