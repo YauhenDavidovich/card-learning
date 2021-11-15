7938dc08c992674200929c1bc0638b95e49fed2a
IngvarrFX <64922169+IngvarrFX@users.noreply.github.com>
2021-11-15 16:54:13 +0300

    feat: add app reducer

---
+++ src/bll/app-reducer.ts
@@ -0,0 +1,38 @@
+const initialState = {
+    status: "idle",
+    isInitialized: false
+};
+
+export type AppInitialStateType = typeof initialState;
+
+//Reducer
+export const appReducer = (state = initialState, action: any): AppInitialStateType => {
+    switch (action.type) {
+        case "APP/SET-STATUS":
+            return {...state, status: action.status}
+        case "APP/INITIALAZE":
+            return {...state, isInitialized: action.isInitialazed}
+        default:
+            return {...state}
+    }
+};
+
+
+// Action Creators
+export const setAppStatusAC = (status: RequestStatusType) => ({type: "APP/SET-STATUS", status} as const)
+export const setIsInitializedAC = (isInitialazed: boolean) => ({type: "APP/INITIALAZE", isInitialazed} as const)
+
+
+//types
+export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed"
+
+
+type ActionsType =
+    | SetAppStatusActionType
+    | SetAppInitialazeActionType
+
+
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppInitialazeActionType = ReturnType<typeof setIsInitializedAC>
+
+
