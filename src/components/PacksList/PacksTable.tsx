07604da3707ff4d1d6fb6ebc4f5b32623f4b041f
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-21 00:10:42 +0300

    feat: add

--- src/components/PacksList/PacksTable.tsx
+++ src/components/PacksList/PacksTable.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../bll/store";
+import {getCardsTC} from "../../bll/packs-reducer";
 
 
 const PackTable = () => {
+    const cards=useSelector<AppStateType>(state=>state.packs.cardsPacks)
 
-    return (
+const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getCardsTC())
+    },[])
+
+
+    return (<div>
+            {cards}
         <div>PackTable</div>
+        </div>
     )
 }
 
